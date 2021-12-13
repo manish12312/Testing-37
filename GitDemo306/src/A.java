01cf81bdcbfc34f36ee1c0d72a21243752ea8ad5
MANISH <MANISH@DESKTOP-NPV50CU>
2021-12-13 22:34:11 +0530

    by ganu

---
+++ GitDemo306/src/A.java
@@ -0,0 +1,49 @@
+
+public class A 
+{
+	
+	
+	
+	public static void main(String[] args)
+	{
+	
+		A a1=new A();
+		a1.m1();
+		
+		
+	
+	
+	
+	
+	}
+
+
+	public void m1() 
+	{
+		
+		System.out.println(" M1 of A your program ");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
